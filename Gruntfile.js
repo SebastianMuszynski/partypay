77fb1faf336a4673be037336c084eec5341224cf
Sebastian Muszynski <muszynski.sebastian@gmail.com>
2015-11-21 16:52:31 +0000

    Disable caching for images minification.

--- Gruntfile.js
+++ Gruntfile.js
@@ -345,7 +345,10 @@ module.exports = function (grunt) {
           cwd: '<%= yeoman.app %>/images',
           src: '{,*/}*.{png,jpg,jpeg,gif}',
           dest: '<%= yeoman.dist %>/images'
-        }]
+        }],
+        options: {
+          cache: false
+        }
       }
     },
 
